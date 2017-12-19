a5b94d0f9e908196abc195d868aa1909b03b169d
Alexander Kaiser <alexander.kaiser.g@gmail.com>
2017-12-19 20:12:58 +0100

    Renamed init to maintenance module

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,12 @@
 // EmperLex screeps AI
 // Dont try this at home
 
-var init = require('init');
+var maintenance = require('maintenance');
 var patch = require('patch');
 var probes = require('role.probe');
 
 module.exports.loop = function() {
-    init.run();
+    maintenance.run();
     probes.run();
     patch.run();
 }
