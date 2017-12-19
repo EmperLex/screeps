fc7a479e72601f5df0aa0aa4fa46efe1805f73e1
Alexander Kaiser <alexander.kaiser.g@gmail.com>
2017-12-19 19:35:12 +0100

    Minor code clean up

--- dist/init.js
+++ dist/init.js
@@ -1,10 +1,15 @@
 var probes = require('role.probe');
 var util = require('util');
 
+function respawn() {
+  if(util.creepCount() < 3) {
+    probes.spawn();
+  }
+}
+
 module.exports = {
     run : function() {
-        if(util.creepCount() <= 3) {
-            probes.spawn();
-        }
+        respawn();
+        
     }
 };
