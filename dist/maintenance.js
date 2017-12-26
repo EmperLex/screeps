015cf52259f72f5c86a475db42fb18200b70dbae
Alexander Kaiser <alexander.kaiser.g@gmail.com>
2017-12-26 10:57:45 +0100

    Add memory clean up for dead creeps

--- dist/maintenance.js
+++ dist/maintenance.js
@@ -1,15 +1,27 @@
+'use strict';
+
 var probes = require('role.probe');
 var util = require('util');
 
 function respawn() {
-  if(util.creepCount() < 3) {
     probes.spawn();
+}
+
+function freeMemory() {
+  for (var creepKey in Memory.creeps) {
+    if(!Game.creeps[creepKey]) {
+        delete Memory.creeps[creepKey];
+    }
   }
 }
 
+
 module.exports = {
     run : function() {
-        respawn();
-        
+        if(util.creepCount() < 3)
+          respawn();
+
+        if(Game.time % 100 == 0)
+          freeMemory();
     }
 };
