14ed652fc6bdbd89019222b5f11c2c1b3e76545e
Alexander Kaiser <alexander.kaiser.g@gmail.com>
2017-12-29 22:51:28 +0100

    creepctl fine tuning
    - only one upgrader built at max
    - dont exceed limit of 9 creeps to avoid blocks when spawn in full

--- dist/creepctl.js
+++ dist/creepctl.js
@@ -45,22 +45,25 @@ function spawn() {
 
   for (var creepKey in Game.creeps) {
     var src = Game.creeps[creepKey].memory.source;
+    var target = Game.creeps[creepKey].memory.target; //hmpf - kind of hacky...
     tmp[src]--;
+    tmp[target]--;
   }
 
-  for (var src in tmp) {
-    if(tmp[src] > 0) {
+  for (var dest in tmp) {
+    if(tmp[dest] > 0) {
 
       var source = null;
       var target = null;
       var result = null;
 
-      if(Game.getObjectById(src).structureType == STRUCTURE_CONTROLLER) {
+      if(Game.getObjectById(dest).structureType == STRUCTURE_CONTROLLER
+        && Object.keys(Game.creeps).length > 0) {
         source = util.mainSpawn();
-        target = Game.getObjectById(src);
+        target = Game.getObjectById(dest);
         result = upgrader.spawn(util.mainSpawn(), source, target);
       } else {
-        source = Game.getObjectById(src);
+        source = Game.getObjectById(dest);
         target = util.mainSpawn();
         result = probe.spawn(util.mainSpawn(), source, target);
       }
@@ -105,11 +108,8 @@ function calcMaxPaths() {
         return { id: source.id, pos: source.pos, range: 1, baseCost: path.cost };
       });
 
-      goals.push( {
-        id: util.mainSpawn().room.controller.id,
-        pos: util.mainSpawn().room.controller.pos,
-        range: 1,
-        baseCost: 0 } );
+      var ctl = util.mainSpawn().room.controller;
+      goals.push( { id: ctl.id, pos: ctl.pos, range: 1, baseCost: 0, maxPaths: 1 } );
 
       goals.sort(function(a,b) { return a.baseCost - b.baseCost });
 
@@ -158,6 +158,12 @@ function calcMaxPaths() {
         }
         Memory.tmpCosts = costs.serialize();
         Memory.creepCtrl.maxPaths[goal.id]++;
+
+        if(goal.maxPaths != undefined && goal.maxPaths <= Memory.creepCtrl.maxPaths[goal.id]) {
+          var index =  tmpGoals.findIndex(item => item.id === goal.id); //TODO duplicate code
+          tmpGoals.splice(index, 1);
+          Memory.tmpGoals = tmpGoals;
+        }
       }
     }
 }
