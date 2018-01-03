1362d666bb690ecf88abf1f0499d9b6301453b88
Alexander Kaiser <alexander.kaiser.g@gmail.com>
2018-01-03 22:24:54 +0100

    Fix logger constants not found

--- dist/logger.js
+++ dist/logger.js
@@ -1,18 +1,19 @@
 'use strict';
 
-module.exports = {
-  TRACE,
-  DEBUG,
-  INFO,
-  ERROR,
-  OFF
-}
-
 const TRACE = 0;
 const DEBUG = 1;
 const INFO = 2;
 const ERROR = 3;
 const OFF = 255; // only used for global log level
+
+module.exports = {
+  TRACE,
+  DEBUG,
+  INFO,
+  ERROR,
+  OFF,
+  log
+}
 
 function log(msg, level) {
 
